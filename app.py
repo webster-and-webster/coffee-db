f3bb15c20090afd71d7452449b68fddae3ce65b2
nedwebster <edwardpwebster@gmail.com>
2023-03-27 21:09:31 +0100

    add view_data and tables pages

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from coffee_db.app import Site
+<<<<<<< HEAD
 from coffee_db.app.pages import (
     HomePage,
     AddRemoveData,
@@ -6,6 +7,9 @@ from coffee_db.app.pages import (
     WorldMapPlot,
     ViewData,
 )
+=======
+from coffee_db.app.pages import HomePage, AddRemoveData, Visualizations, WorldMapPlot, ViewData
+>>>>>>> 966c821 (add view_data and tables pages)
 from coffee_db import CoffeeDB
 from coffee_db.data_loaders import PostgresDataLoader
 
@@ -17,12 +21,21 @@ pages = [
     Visualizations(tabs=[WorldMapPlot()]),
     ViewData(
         tabs=[
+<<<<<<< HEAD
             tables.StreamlitTable("roastery"),
             tables.StreamlitTable("coffee"),
             tables.StreamlitTable("country"),
             tables.StreamlitTable("variety"),
             tables.StreamlitTable("process"),
             tables.StreamlitTable("coffee_user"),
+=======
+            tables.RoasteryTable(),
+            tables.CoffeeTable(),
+            tables.CountryTable(),
+            tables.VarietyTable(),
+            tables.ProcessTable(),
+            tables.CoffeeUserTable(),
+>>>>>>> 966c821 (add view_data and tables pages)
         ]
     ),
     AddRemoveData(
