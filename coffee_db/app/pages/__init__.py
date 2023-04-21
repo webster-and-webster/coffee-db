2000df747c79d7afe57af088b600535c9f6d0dc0
Ned Webster <49588411+nedwebster@users.noreply.github.com>
2023-04-21 18:07:22 +0100

    Dev/add coffee users plot (#14)
    * add datetime plot code:
    
    * add visualisation to app
    
    * fix linting issues
    
    * add some tests for datetime plotter
    
    * update requirements.txt for heroku deployment
    
    * bump version to 1.1.1

--- coffee_db/app/pages/__init__.py
+++ coffee_db/app/pages/__init__.py
@@ -2,5 +2,9 @@
 
 from coffee_db.app.pages.home_page import HomePage
 from coffee_db.app.pages.add_remove_data import AddRemoveData
-from coffee_db.app.pages.visualizations import Visualizations, WorldMapPlot
+from coffee_db.app.pages.visualizations import (
+    CoffeesByUser,
+    Visualizations,
+    WorldMapPlot,
+)
 from coffee_db.app.pages.view_data import ViewData
