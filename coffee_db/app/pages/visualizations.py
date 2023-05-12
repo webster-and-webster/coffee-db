a36951290ebcebaa654e5a864289043d741af569
Ned Webster <edwardpwebster@gmail.com>
2023-05-12 16:11:52 +0100

    add roasteries_by_country visualization

--- coffee_db/app/pages/visualizations.py
+++ coffee_db/app/pages/visualizations.py
@@ -3,7 +3,10 @@ from streamlit_folium import folium_static
 
 from coffee_db.app.utils import Page, Tab
 from coffee_db.visualizations.datetime_plot import DatetimePlotter
-from coffee_db.visualizations.map_visualizations import plot_coffees_by_country
+from coffee_db.visualizations.map_visualizations import (
+    plot_coffees_by_country,
+    plot_roasteries_by_country,
+)
 
 
 class Visualizations(Page):
@@ -19,7 +22,15 @@ class WorldMapPlot(Tab):
 
     def write(self):
         st.title(self.header)
-        folium_static(plot_coffees_by_country(coffees=st.cache.coffees))
+        option = st.selectbox(
+            "plot_type",
+            ("Coffees", "Roasteries"),
+            label_visibility="hidden",
+        )
+        if option == "Coffees":
+            folium_static(plot_coffees_by_country(coffees=st.cache.coffees))
+        else:
+            folium_static(plot_roasteries_by_country(roasteries=st.cache.roasteries))
 
 
 class CoffeesByUser(Tab):
